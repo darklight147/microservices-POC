0ef81fc43bf39d84cef037dc04bc338143dbe05a
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-07-05 16:23:42 +0100

    fix: signup client

--- client/pages/auth/signup-visitor.jsx
+++ client/pages/auth/signup-visitor.jsx
@@ -1,15 +1,62 @@
 import { useRequest } from '../../hooks/use-request';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 function SignUpVisitor() {
-	const {} = useRequest();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 
-	const handleSubmit = (e) => {
+	const router = useRouter();
+
+	const { doRequest, errors } = useRequest({
+		method: 'post',
+		url: '/api/auth/signup/visitor',
+		body: { username: email, password },
+		onSuccess: () => {
+			router.push('/profile');
+		},
+	});
+
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+		await doRequest();
 	};
 
 	return (
 		<div>
-			<form onSubmit={handleSubmit}></form>
+			<form onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="email">Email</label>
+					<input
+						type="email"
+						className="form-control"
+						id="email"
+						placeholder="Enter email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						type="password"
+						className="form-control"
+						id="password"
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<button type="submit" className="btn btn-primary" value={'Submit'} />
+				</div>
+			</form>
+
+			{errors && errors.length > 0 && (
+				<div className="alert alert-danger">
+					<ul className="mb-0">
+						{errors.map((error) => (
+							<li key={error}>{error}</li>
+						))}
+					</ul>
+				</div>
+			)}
 		</div>
 	);
 }
