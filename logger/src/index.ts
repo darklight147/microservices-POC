6e7be62abfa45dd2777a185d715f816a9038117f
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-07-08 00:57:01 +0100

    feat: check vars before starting

--- logger/src/index.ts
+++ logger/src/index.ts
@@ -1,7 +1,10 @@
+import { checkVars } from './config/check-vars';
 import rabbitWrapper from './config/rabbitWrapper';
 import { LogListener } from './events/consumers/LogListener';
 
 async function start() {
+	checkVars();
+
 	new LogListener(rabbitWrapper.connection).listen();
 }
 
