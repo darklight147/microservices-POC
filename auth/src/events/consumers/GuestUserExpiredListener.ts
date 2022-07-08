e09f5060776534a3d88eca0c5b12780b13299d45
darklight147 <39389636+darklight147@users.noreply.github.com>
2022-07-08 14:13:37 +0100

    feat: add logger to expired user consumer

--- auth/src/events/consumers/GuestUserExpiredListener.ts
+++ auth/src/events/consumers/GuestUserExpiredListener.ts
@@ -3,25 +3,28 @@ import { ConsumeMessage, Message } from 'amqplib';
 import userService from '../../services/user.service';
 import { parseMessage } from '../../utils/parse-message';
 import { Listener } from '@quasimodo147/common';
+import log from '../../utils/logger';
 
 export class GuestUserExpiredListener extends Listener {
 	queueName: string = GuestQueues.GUEST_USER_EXPIRED;
 
 	onMessage = async (msg: ConsumeMessage | null) => {
-		if (msg) {
-			const data = await parseMessage(msg);
+		if (!msg) return;
 
-			if (data) {
-				const user = await userService.findById(data.userId);
+		const data = await parseMessage(msg);
 
-				await user.remove();
+		if (data) {
+			const user = await userService.findById(data.userId);
 
-				this.channel.ack(msg);
-				return;
-			}
+			if (!user) this.channel.ack(msg);
 
+			await user.remove();
+
+			this.channel.ack(msg);
+
+			log.info(`Guest user ${user.id} expired and removed from db`);
+		} else {
 			this.channel.nack(msg, false, false);
-			return;
 		}
 	};
 }
